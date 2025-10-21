7eb99ff30a2e8634368868e0f6fa84903539c02f
Wong Yui Pan <e1562707@u.nus.edu>
2025-10-22 00:40:20 +0800

    Set base path for Vite server configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8000
-  }
+  server: { port: 8000 },
+  base: '/askba14/',
 })
