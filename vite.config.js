f680d08258bcc62f5001ea979674fe45fa9f5bec
Wong Yui Pan <e1562707@u.nus.edu>
2025-10-22 01:18:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: { port: 8000 },
   base: '/askba14/',
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        grid:  resolve(__dirname, 'grid.html'),
+      },
+    },
+  },
 })
